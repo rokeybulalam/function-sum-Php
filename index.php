2641b203c7e413898bc39a4df2394aaf8276166d
rokeybulalam <rokeybulalam1122@gmail.com>
2023-06-18 22:02:07 +0600

    first commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+
+function sum($number1, $number2){
+    return $number1 * $number2;
+}
+
+echo sum(10,10) +10;
+echo '<br/>';
+echo sum(70,10) -100;
+echo '<br/>';
+echo sum(75,75);
+echo '<br/>';
